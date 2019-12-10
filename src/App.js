0f97448de2b88e062edd64041081680b71c77459
Burkay SEVİLMİŞ <burkay.sevilmis@gmail.com>
2019-12-10 14:30:55 +0300

    Firebase Authentication

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
 import React, { Component } from 'react';
-import { View, Text,StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import Banner from './components/banner';
 import Form from './components/loginForm';
+import firebase from 'firebase'
+import loginForm from './components/loginForm';
+import Spinner from './components/common';
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      loggedIn: false
     };
   }
 
+  componentDidMount() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyCkm0vTnosgv8UtKOzs9GwlmYBKqv28GYA",
+      authDomain: "giris-6c512.firebaseapp.com",
+      databaseURL: "https://giris-6c512.firebaseio.com",
+      projectId: "giris-6c512",
+      storageBucket: "giris-6c512.appspot.com",
+      messagingSenderId: "812577739876",
+      appId: "1:812577739876:web:c467f842bc951c1fa17b71",
+      measurementId: "G-PMY60CWDJY"
+    });
+    firebase.auth().onAuthStateChanged((user) => {
+      const loggedIn = user ? true : false;
+      this.setState({ loggedIn });
+    })
+  }
+  renderContent() {
+    const { loggedIn } = this.state;
+    switch (loggedIn) {
+      case true:
+        return (
+          <TouchableOpacity onPress={()=>firebase.auth().signOut()} style={{ marginTop: 10, width: '100%', height: 45, backgroundColor: 'white', borderRadius: 12, borderWidth: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text style={{ fontSize: 20, color: '#e87b79' }}>Log Out</Text></TouchableOpacity>
+        )
+    
+      case false:
+        return (
+          <Form/>
+          )
+          default:
+            return(
+              <Spinner/>
+            )
+    }
+
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Banner text="Kullanıcı Girişi"/>
-        <Form/>
+        <Banner text="Kullanıcı Girişi" />
+        {this.renderContent()}
       </View>
     );
   }
 }
-const styles=StyleSheet.create({
-  container:{
-    flex:1,
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
   }
 })
--- src/components/loginForm.js
+++ src/components/loginForm.js
@@ -1,33 +1,72 @@
 import React, { Component } from 'react';
-import { View, Text,TouchableOpacity,StyleSheet} from 'react-native';
-import {Input} from './common';
+import { View, Text, TouchableOpacity, StyleSheet, ActivityIndicator } from 'react-native';
+import { Input, Spinner } from './common';
+import firebase from 'firebase';
 export default class loginForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-        email:'',
-        password:'',
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: '',
+            error: '',
+            loading: false
+        };
+    }
 
-  render() {
-    return (
-      <View style={styles.LoginBox}>
-        <View>
-            <Input value={this.state.email} onChangeText={(text)=>{this.setState({email:text})}} text='Email' inputPlaceHolder='Eposta Giriniz'/>
-        </View>
-        <View>
-            <Input secureTextEntry value={this.state.password} onChangeText={(text)=>{this.setState({password:text})}}  text='Şifre'/>
-        </View>
-        <TouchableOpacity style={{marginTop:10,width:'100%',height:45,backgroundColor:'white',borderRadius:12,borderWidth:1,justifyContent:'center',alignItems:'center'}}>
-            <Text style={{fontSize:20,color:'#e87b79'}}>Giriş</Text></TouchableOpacity>
-      </View>
-    );
-  }
+    onButtonClicked() {
+        const { email, password } = this.state;
+        this.setState({ error: '' ,loading:true})
+        firebase.auth().signInWithEmailAndPassword(email, password)
+        .then(this.onLoginSuccess.bind(this))
+            .catch((error) => {
+                console.log(error)
+                firebase.auth().createUserWithEmailAndPassword(email, password)
+                    .catch((error) => {
+                        console.log(error);
+                        this.setState({
+                            error: 'Authentication Error',
+                            loading:false
+                        })
+                    })
+            })
+    }
+    onLoginSuccess(){
+        this.setState({
+            email:'',
+            password:'',
+            loading:false,
+            error:'',
+        })
+    }
+    render() {
+        const { error, loading } = this.state;
+        const errorMsg = error ? (
+            <Text style={{ fontSize: 20, color: 'red', alignSelf: 'center', marginTop: 10 }}>{error}</Text>
+        ) :
+            null;
+        const loginButton = loading ? (
+            
+            <Spinner />
+        ) :
+            <TouchableOpacity onPress={this.onButtonClicked.bind(this)} style={{ marginTop: 10, width: '100%', height: 45, backgroundColor: 'white', borderRadius: 12, borderWidth: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <Text style={{ fontSize: 20, color: '#e87b79' }}>Giriş</Text></TouchableOpacity>;
+        return (
+            <View style={styles.LoginBox}>
+                <View>
+                    <Input value={this.state.email} onChangeText={(text) => { this.setState({ email: text }) }} text='Email' inputPlaceHolder='Eposta Giriniz' />
+                </View>
+                <View>
+                    <Input secureTextEntry value={this.state.password} onChangeText={(text) => { this.setState({ password: text }) }} text='Şifre' />
+                </View>
+                {errorMsg}
+                    {loginButton}
+            </View>
+        );
+    }
 }
 
-const styles =StyleSheet.create({
-LoginBox:{
-    padding:10,
-}
+const styles = StyleSheet.create({
+    LoginBox: {
+        padding: 10,
+    }
 });
